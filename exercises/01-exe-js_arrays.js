3a6940d5fa0eabbb4d9d5fa5ac4146a574dc7f96
Marco Santonastasi <marco.santonastasi@gmail.com>
2022-01-17 22:43:50 +0100

    Update 01-exe-js_arrays.js

--- exercises/01-exe-js_arrays.js
+++ exercises/01-exe-js_arrays.js
@@ -1,1 +1,28 @@
-// Practice
+// Practice JS Arrays
+
+//Given the array
+const favorites = ['javascript', 'html', 'css'];
+
+// remove first element
+// ... your code here
+console.log(favorites); // => [ 'html', 'css' ]
+
+// remove last element
+// ... your code here
+console.log(favorites); // => [ 'html' ]
+
+// add 'react' on the first place
+// ... your code here
+console.log(favorites); // => [ 'react', 'html' ]
+
+// add 'node' to the last place
+// ... your code here
+console.log(favorites); // => [ 'react', 'html', 'node' ]
+
+// remove element in the position 2 and add 'express'
+// ... your code here
+console.log(favorites); // => [ 'react', 'express', 'node' ]
+
+// remove elements in the positions 2 and 3 and add 'mongodb'
+// ... your code here
+console.log(favorites); // => [ 'react', 'mongodb' ]
